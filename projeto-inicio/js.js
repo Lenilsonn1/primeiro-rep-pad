4262dcbc82178a1bd95087d4cf1bd4b6babb9bb1
Lenilsonn1 <lenilsonliralucas@gmail.com>
2022-11-08 18:00:43 -0300

    Primeiros comandos JS

---
+++ projeto-inicio/js.js
@@ -0,0 +1,9 @@
+var missão = 'Missão Estelar em Execução'
+console.log(missão)
+
+function executarOnload(){
+    console.log('Evento Onload DISPARADO!!')
+}
+function executarOnclick(){
+    console.log('Missão Estelar Realizada Com Sucesso!!')
+}
